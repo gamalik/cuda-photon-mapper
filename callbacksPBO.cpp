77ec1dd950867a4b85f9dd97b68c33705f2bfa43
resendizjose <resendizjose@a711a315-6140-9f10-c135-3ab47e8c5f68>
2010-09-23 13:02:56 +0000

    Photon emision in cpu, ray tracing in gpu, photon gathering pending in gpu.

--- callbacksPBO.cpp
+++ callbacksPBO.cpp
@@ -22,8 +22,367 @@ extern unsigned int image_height;
 // The user must create the following routines:
 void runCuda();
 
+
+
+////////////////////////////////////////////////////////////////////////////
+// Photon Mapping Code
+
+
+
+// ----- Scene Description -----
+
+int szImg = 512;                  //Image Size
+int nrTypes = 2;                  //2 Object Types (Sphere = 0, Plane = 1)
+
+int nrObjects[] = {2,5};          //2 Spheres, 5 Planes
+float gAmbient = 0.1;             //Ambient Lighting
+
+float gOrigin[] = {0.0,0.0,0.0};  //World Origin for Convenient Re-Use Below (Constant)
+float Light[] = {0.0,1.2,3.75};   //Point Light-Source Position
+
+float spheres[][4] = {{1.0,0.0,4.0,0.5},{-0.6,-1.0,4.5,0.5}};         //Sphere Center & Radius
+float planes[][2]  = {{0, 1.5},{1, -1.5},{0, -1.5},{1, 1.5},{2,5.0}}; //Plane Axis & Distance-to-Origin
+
+
+// ----- Photon Mapping -----
+int nrPhotons = 1000;             //Number of Photons Emitted
+
+int nrBounces = 3;                //Number of Times Each Photon Bounces
+boolean lightPhotons = true;      //Enable Photon Lighting?
+
+float sqRadius = 0.7;             //Photon Integration Area (Squared for Efficiency)
+float exposure = 50.0;            //Number of Photons Integrated at Brightest Pixel
+
+int numPhotons [][5] = {{0,0},{0,0,0,0,0}};              //Photon Count for Each Scene Object
+float *photons[2][5][5000][3]; // = new float[2][5][5000][3][3]; //Allocated Memory for Per-Object Photon Info
+
+
+// ----- Raytracing Globals -----
+boolean gIntersect = false;       //For Latest Raytracing Call... Was Anything Intersected by the Ray?
+
+int gType;                        //... Type of the Intersected Object (Sphere or Plane)
+int gIndex;                       //... Index of the Intersected Object (Which Sphere/Plane Was It?)
+
+float gSqDist, gDist = -1.0;      //... Distance from Ray Origin to Intersection
+float * gPoint = new float[3];
+//{0.0f, 0.0f, 0.0f}; //... Point At Which the Ray Intersected the Object
+
+
+
+
+//Vector Operations ---------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+float* mul3c(float a[], float c){    //Multiply 3-Vector with Scalar
+
+  float result[] = {c*a[0], c*a[1], c*a[2]};
+  return result;
+}
+
+float dot3(float a[], float b[]){     //Dot Product 3-Vectors
+
+  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+float* normalize3(float v[]){        //Normalize 3-Vector
+  float L = sqrt(dot3(v,v));
+
+  return mul3c(v, 1.0/L);
+}
+
+float* sub3(float a[], float b[]){   //Subtract 3-Vectors
+
+  float result[] = {a[0] - b[0], a[1] - b[1], a[2] - b[2]};
+  return result;
+}
+
+float* add3(float a[], float b[]){   //Add 3-Vectors
+
+  float result[] = {a[0] + b[0], a[1] + b[1], a[2] + b[2]};
+  return result;
+}
+
+float random(float min, float max) {
+	int randInt = rand();
+	
+	// rand [0..1]
+	float randFloat = (float) randInt / (float) RAND_MAX;
+
+	// rand [0..max-min]
+	randFloat *= (max-min);
+
+	// rand [min..max]
+	randFloat += min;
+
+	return randFloat;
+
+}
+
+float* rand3(float s){               //Random 3-Vector
+
+  float rand[] = {random(-s,s),random(-s,s),random(-s,s)};
+
+  return rand;
+}
+
+bool gatedSqDist3(float a[], float b[], float sqradius){ //Gated Squared Distance
+
+  float c = a[0] - b[0];          //Efficient When Determining if Thousands of Points
+  float d = c*c;                  //Are Within a Radius of a Point (and Most Are Not!)
+
+  if (d > sqradius) return false; //Gate 1 - If this dimension alone is larger than
+
+  c = a[1] - b[1];                //         the search radius, no need to continue
+  d += c*c;
+  if (d > sqradius) return false; //Gate 2
+
+  c = a[2] - b[2];
+  d += c*c;
+  if (d > sqradius) return false; //Gate 3
+
+  gSqDist = d;      return true ; //Store Squared Distance Itself in Global State
+
+}
+
+
+
+
+
+//---------------------------------------------------------------------------------------
+//Ray-Geometry Intersections  -----------------------------------------------------------
+
+//---------------------------------------------------------------------------------------
+
+void checkDistance(float lDist, int p, int i){
+
+  if (lDist < gDist && lDist > 0.0){ //Closest Intersection So Far in Forward Direction of Ray?
+    gType = p; gIndex = i; gDist = lDist; gIntersect = true;} //Save Intersection in Global State
+
+}
+
+
+void raySphere(int idx, float r[], float o[]) //Ray-Sphere Intersection: r=Ray Direction, o=Ray Origin
+
+{ 
+  float * s = sub3(spheres[idx],o);  //s=Sphere Center Translated into Coordinate Frame of Ray Origin
+  float radius = spheres[idx][3];    //radius=Sphere Radius
+
+  
+  //Intersection of Sphere and Line     =       Quadratic Function of Distance
+  float A = dot3(r,r);                       // Remember This From High School? :
+
+  float B = -2.0 * dot3(s,r);                //    A x^2 +     B x +               C  = 0
+  float C = dot3(s,s) - pow(radius, 2.0f);          // (r'r)x^2 - (2s'r)x + (s's - radius^2) = 0
+
+  float D = B*B - 4*A*C;                     // Precompute Discriminant
+  
+  if (D > 0.0){                              //Solution Exists only if sqrt(D) is Real (not Imaginary)
+
+    float sign = (C < -0.00001) ? 1 : -1;    //Ray Originates Inside Sphere If C < 0
+    float lDist = (-B + sign*sqrt(D))/(2*A); //Solve Quadratic Equation for Distance to Intersection
+
+    checkDistance(lDist,0,idx);}             //Is This Closest Intersection So Far?
+}
+
+void rayPlane(int idx, float r[], float o[]){ //Ray-Plane Intersection
+
+  int axis = (int) planes[idx][0];            //Determine Orientation of Axis-Aligned Plane
+
+  if (r[axis] != 0.0){                        //Parallel Ray -> No Intersection
+    float lDist = (planes[idx][1] - o[axis]) / r[axis]; //Solve Linear Equation (rx = p-o)
+
+    checkDistance(lDist,1,idx);}
+}
+
+void rayObject(int type, int idx, float r[], float o[]){
+
+  if (type == 0) raySphere(idx,r,o); else rayPlane(idx,r,o);
+}
+
+float min(float a, float b) {
+	if(a <= b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+float max(float a, float b) {
+	if(a >= b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+
+//---------------------------------------------------------------------------------------
+// Lighting -----------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------
+
+float lightDiffuse(float N[], float P[]){  //Diffuse Lighting at Point P with Surface Normal N
+
+  float * L = normalize3( sub3(Light,P) ); //Light Vector (Point to Light)
+  return dot3(N,L);                        //Dot Product = cos (Light-to-Surface-Normal Angle)
+
+}
+
+float* sphereNormal(int idx, float* P){
+  return normalize3(sub3(P,spheres[idx])); //Surface Normal (Center to Point)
+
+}
+
+float* planeNormal(int idx, float* P, float* O){
+
+  int axis = (int) planes[idx][0];
+  float N[] = {0.0f,0.0f,0.0f};
+  N[axis] = O[axis] - planes[idx][1];      //Vector From Surface to Light
+
+  return normalize3(N);
+}
+
+float* surfaceNormal(int type, int index, float* P, float* Inside){
+
+  if (type == 0) {return sphereNormal(index,P);}
+  else           {return planeNormal(index,P,Inside);}
+
+}
+
+float lightObject(int type, int idx, float* P, float lightAmbient){
+
+  float i = lightDiffuse( surfaceNormal(type, idx, P, Light) , P );
+  return min(1.0, max(i, lightAmbient));   //Add in Ambient Light by Constraining Min Value
+
+}
+
+float* filterColor(float* rgbIn, float r, float g, float b){ //e.g. White Light Hits Red Wall
+
+  float rgbOut[] = {r,g,b};
+  for (int c=0; c<3; c++) rgbOut[c] = min(rgbOut[c],rgbIn[c]); //Absorb Some Wavelengths (R,G,B)
+
+  return rgbOut;
+}
+
+float* getColor(float rgbIn[], int type, int index){ //Specifies Material Color of Each Object
+
+  if      (type == 1 && index == 0) { return filterColor(rgbIn, 0.0, 1.0, 0.0);}
+  else if (type == 1 && index == 2) { return filterColor(rgbIn, 1.0, 0.0, 0.0);}
+
+  else                              { return filterColor(rgbIn, 1.0, 1.0, 1.0);}
+}
+
+
+//---------------------------------------------------------------------------------------
+// Raytracing ---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------
+
+void raytrace(float* ray, float* origin)
+
+{
+  gIntersect = false; //No Intersections Along This Ray Yet
+  gDist = 999999.9;   //Maximum Distance to Any Object
+
+  
+  for (int t = 0; t < nrTypes; t++)
+    for (int i = 0; i < nrObjects[t]; i++)
+
+      rayObject(t,i,ray,origin);
+}
+
+
+void storePhoton(int type, int id, float* location, float* direction, float* energy){
+
+  photons[type][id][numPhotons[type][id]][0] = location;  //Location
+  photons[type][id][numPhotons[type][id]][1] = direction; //Direction
+
+  photons[type][id][numPhotons[type][id]][2] = energy;    //Attenuated Energy (Color)
+  numPhotons[type][id]++;
+}
+
+void shadowPhoton(float* ray){                               //Shadow Photons
+
+  float shadow[] = {-0.25,-0.25,-0.25};
+  float* tPoint = gPoint; 
+  int tType = gType, tIndex = gIndex;                         //Save State
+
+  float *bumpedPoint = add3(gPoint,mul3c(ray,0.00001));      //Start Just Beyond Last Intersection
+  raytrace(ray, bumpedPoint);                                 //Trace to Next Intersection (In Shadow)
+
+  float* shadowPoint = add3( mul3c(ray,gDist), bumpedPoint); //3D Point
+  storePhoton(gType, gIndex, shadowPoint, ray, shadow);
+
+  gPoint = tPoint; gType = tType; gIndex = tIndex;            //Restore State
+}
+
+
+float* reflect(float* ray, float* fromPoint){                //Reflect Ray
+
+  float* N = surfaceNormal(gType, gIndex, gPoint, fromPoint);  //Surface Normal
+  return normalize3(sub3(ray, mul3c(N,(2 * dot3(ray,N)))));     //Approximation to Reflection
+
+}
+
+
+void emitPhotons(){
+	
+	// randomSeed(0);                               //Ensure Same Photons Each Time, really needed?
+
+  for (int t = 0; t < nrTypes; t++)            //Initialize Photon Count to Zero for Each Object
+    for (int i = 0; i < nrObjects[t]; i++)
+      numPhotons[t][i] = 0; 
+
+  for (int i = 0; i < nrPhotons * 3.0; i++){ //Draw 3x Photons For Usability
+
+    int bounces = 1;
+    float * rgb = new float[3];
+	
+	rgb[0]=1.0;
+	rgb[1]=1.0;
+	rgb[2]=1.0;
+
+	//{1.0,1.0,1.0};               //Initial Photon Color is White
+
+    float * ray = normalize3( rand3(1.0) );    //Randomize Direction of Photon Emission
+    float * prevPoint = Light;                 //Emit From Point Light Source
+
+    
+    //Spread Out Light Source, But Don't Allow Photons Outside Room/Inside Sphere
+    while (prevPoint[1] >= Light[1]){ prevPoint = add3(Light, mul3c(normalize3(rand3(1.0)), 0.75));}
+
+    if (abs(prevPoint[0]) > 1.5 || abs(prevPoint[1]) > 1.2 ||
+        gatedSqDist3(prevPoint,spheres[0],spheres[0][3]*spheres[0][3])) bounces = nrBounces+1;
+
+    
+    raytrace(ray, prevPoint);                          //Trace the Photon's Path
+    
+    while (gIntersect && bounces <= nrBounces){        //Intersection With New Object
+
+        gPoint = add3( mul3c(ray,gDist), prevPoint);   //3D Point of Intersection
+        rgb = mul3c (getColor(rgb,gType,gIndex), 1.0/sqrt((float)bounces));
+
+        storePhoton(gType, gIndex, gPoint, ray, rgb);  //Store Photon Info 
+        // drawPhoton(rgb, gPoint);                       //Draw Photon, skipped
+
+        shadowPhoton(ray);                             //Shadow Photon
+        ray = reflect(ray,prevPoint);                  //Bounce the Photon
+
+        raytrace(ray, gPoint);                         //Trace It to Next Location
+        prevPoint = gPoint;
+        bounces++;}
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
 void display()
 {
+  
+  emitPhotons();
+	
   // run CUDA kernel
   runCuda();
   
--- kernelPBO.cu
+++ kernelPBO.cu
@@ -1,6 +1,7 @@
 //kernelPBO.cu (Rob Farber)
 
 #include <stdio.h>
+#include <cutil_math.h>
 
 void checkCUDAError(const char *msg) {
   cudaError_t err = cudaGetLastError();
@@ -9,6 +10,164 @@ void checkCUDAError(const char *msg) {
     exit(EXIT_FAILURE); 
   }
 } 
+
+
+
+__device__ void checkDistance(float lDist, int p, int i, bool * gIntersect, int * gType, int * gIndex, float * gDist){
+
+  if (lDist < *gDist && lDist > 0.0){ //Closest Intersection So Far in Forward Direction of Ray?
+    *gType = p; *gIndex = i; *gDist = lDist; *gIntersect = true; //Save Intersection in Global State
+  }
+}
+
+
+
+__device__ void raySphere(int idx, float3 r, float3 o, float4 * spheres,
+						  bool * gIntersect, int * gType, int * gIndex, float * gDist) //Ray-Sphere Intersection: r=Ray Direction, o=Ray Origin
+{ 
+  float3 s = make_float3(spheres[idx].x, spheres[idx].y, spheres[idx].z) - o;  //s=Sphere Center Translated into Coordinate Frame of Ray Origin
+  float radius = spheres[idx].w;    //radius=Sphere Radius
+
+  
+  //Intersection of Sphere and Line     =       Quadratic Function of Distance
+  float A = dot(r,r);                       // Remember This From High School? :
+
+  float B = -2.0 * dot(s,r);                //    A x^2 +     B x +               C  = 0
+  float C = dot(s,s) - pow(radius, 2.0f);          // (r'r)x^2 - (2s'r)x + (s's - radius^2) = 0
+
+  float D = B*B - 4*A*C;                     // Precompute Discriminant
+  
+  if (D > 0.0){                              //Solution Exists only if sqrt(D) is Real (not Imaginary)
+
+    float sign = (C < -0.00001) ? 1 : -1;    //Ray Originates Inside Sphere If C < 0
+    float lDist = (-B + sign*sqrt(D))/(2*A); //Solve Quadratic Equation for Distance to Intersection
+
+    checkDistance(lDist,0,idx,gIntersect,gType,gIndex,gDist);             //Is This Closest Intersection So Far?
+  }
+}
+
+
+__device__ void rayPlane(int idx, float3 r, float3 o, float2 * planes,
+						 bool * gIntersect, int * gType, int * gIndex, float * gDist){ //Ray-Plane Intersection
+
+  int axis = (int) planes[idx].x;            //Determine Orientation of Axis-Aligned Plane
+
+  switch(axis) {
+	case 0:
+		if (r.x != 0.0){                        //Parallel Ray -> No Intersection
+			float lDist = (planes[idx].y - o.x) / r.x; //Solve Linear Equation (rx = p-o)
+			checkDistance(lDist,1,idx,gIntersect,gType,gIndex,gDist);
+		}
+		break;
+	case 1:
+		if (r.y != 0.0){                        //Parallel Ray -> No Intersection
+			float lDist = (planes[idx].y - o.y) / r.y; //Solve Linear Equation (rx = p-o)
+			checkDistance(lDist,1,idx,gIntersect,gType,gIndex,gDist);
+		}
+		break;
+	case 2:
+		if (r.z != 0.0){                        //Parallel Ray -> No Intersection
+			float lDist = (planes[idx].y - o.z) / r.z; //Solve Linear Equation (rx = p-o)
+			checkDistance(lDist,1,idx,gIntersect,gType,gIndex,gDist);
+		}
+		break;
+  }
+
+  
+}
+
+__device__ void rayObject(int type, int idx, float3 r, float3 o, float4 * spheres, float2 * planes,
+						  bool * gIntersect, int * gType, int * gIndex, float * gDist){
+	if (type == 0) 
+		raySphere(idx,r,o,spheres,gIntersect,gType,gIndex,gDist); 
+	else 
+		rayPlane(idx,r,o,planes,gIntersect,gType,gIndex,gDist);
+}
+
+__device__ void raytrace(float3 ray, float3 origin, int nrTypes, int * nrObjects, float4 * spheres, float2 * planes,
+						 bool * gIntersect, int * gType, int * gIndex, float * gDist)
+{
+  
+	*gIntersect = false; //No Intersections Along This Ray Yet
+	*gDist = 999999.9;   //Maximum Distance to Any Object
+
+	for (int t = 0; t < nrTypes; t++)
+		for (int i = 0; i < nrObjects[t]; i++)
+		  rayObject(t,i,ray,origin,spheres,planes,gIntersect,gType,gIndex,gDist);
+
+}
+
+
+__device__ float3 sphereNormal(int idx, float3 P, float4 * spheres){
+  return normalize((P - make_float3(spheres[idx].x, spheres[idx].y, spheres[idx].z))); //Surface Normal (Center to Point)
+
+}
+
+__device__ float3 planeNormal(int idx, float3 P, float3 O, float2 * planes){
+
+  int axis = (int) planes[idx].x;
+  float3 N = make_float3(0.0,0.0,0.0);
+
+  switch(axis) {
+	case 0:
+		N.x = O.x - planes[idx].y;      //Vector From Surface to Light
+		break;
+	case 1:
+		  N.y = O.y - planes[idx].y;
+		break;
+	case 2:
+		  N.z = O.z - planes[idx].y;
+		break;
+  }
+  
+  return normalize(N);
+}
+
+__device__ float3 surfaceNormal(int type, int index, float3 P, float3 Inside, float2 * planes, float4 * spheres){
+
+  if (type == 0) {return sphereNormal(index,P, spheres);}
+  else           {return planeNormal(index,P,Inside,planes);}
+
+}
+
+
+__device__ float3 reflect(float3 ray, float3 fromPoint, int * gType, int * gIndex, float3 * gPoint, float2 * planes, float4 * spheres){                //Reflect Ray
+
+  float3 N = surfaceNormal(*gType, *gIndex, *gPoint, fromPoint, planes, spheres);  //Surface Normal
+  return normalize(ray - (N * (2 * dot(ray,N))));     //Approximation to Reflection
+
+}
+
+
+__device__ float3 computePixelColor(unsigned int x, unsigned int y, float szImg, float3 gOrigin, int nrTypes, int * nrObjects,
+									float4 * spheres, float2 * planes, bool * gIntersect, int * gType, int * gIndex, float * gDist, 
+									float3 * gPoint) {
+	float3 rgb = make_float3(0.0f, 0.0f, 0.0f);
+	float3 ray = make_float3(  x/szImg - 0.5f ,-(y/szImg - 0.5f), 1.0f);	//Convert Pixels to Image Plane Coordinates
+																			//Focal Length = 1.0
+	raytrace(ray, gOrigin, nrTypes, nrObjects, spheres, planes, gIntersect, gType, gIndex, gDist);
+
+	if(*gIntersect) {
+		*gPoint = ray * (*gDist);
+
+		if (*gType == 0 && *gIndex == 1){      //Mirror Surface on This Specific Object
+			ray = reflect(ray,gOrigin,gType,gIndex,gPoint,planes,spheres);        //Reflect Ray Off the Surface
+
+			raytrace(ray, *gPoint, nrTypes, nrObjects, spheres, 
+					planes, gIntersect, gType, gIndex, gDist);             //Follow the Reflected Ray
+
+			if (gIntersect){ 
+				*gPoint =  (ray * (*gDist)) + (*gPoint); 
+			}
+		} //3D Point of Intersection
+
+
+		// rgb = gatherPhotons(gPoint,gType,gIndex);
+
+	}
+
+	return rgb;
+}
 
 //Simple kernel writes changing colors to a uchar4 array
 __global__ void kernel(uchar4* pos, unsigned int width, unsigned int height, 
@@ -19,9 +178,27 @@ __global__ void kernel(uchar4* pos, unsigned int width, unsigned int height,
   unsigned int y = index/width;
 
   if(index < width*height) {
-    unsigned char r = (x + (int) time)&0xff;
-    unsigned char g = (y + (int) time)&0xff;
-    unsigned char b = ((x+y) + (int) time)&0xff;
+   
+    float3 gOrigin = make_float3(0.0f,0.0f,0.0f);
+	int nrTypes = 2; 
+	int nrObjects[] = {2,5};          //2 Spheres, 5 Planes
+	float4 spheres[] = {make_float4(1.0,0.0,4.0,0.5),make_float4(-0.6,-1.0,4.5,0.5)}; //Sphere Center & Radius
+	float2 planes[] = {make_float2(0, 1.5),make_float2(1, -1.5),make_float2(0, -1.5),make_float2(1, 1.5),make_float2(2,5.0)}; //Plane Axis & Distance-to-Origin
+
+	// ----- Raytracing Globals -----
+	bool gIntersect = false;       //For Latest Raytracing Call... Was Anything Intersected by the Ray?
+
+	int gType;                        //... Type of the Intersected Object (Sphere or Plane)
+	int gIndex;                       //... Index of the Intersected Object (Which Sphere/Plane Was It?)
+
+	float gSqDist, gDist = -1.0;      //... Distance from Ray Origin to Intersection
+	float3 gPoint = make_float3(0.0, 0.0, 0.0); //... Point At Which the Ray Intersected the Object
+
+    float3 pixelColor = computePixelColor(x,y,width, gOrigin, nrTypes, nrObjects, spheres, planes, &gIntersect, &gType, &gIndex, &gDist, &gPoint);
+
+	unsigned char r = (unsigned char)(pixelColor.x * 255.0f);
+    unsigned char g = (unsigned char)(pixelColor.y * 255.0f);
+    unsigned char b = (unsigned char)(pixelColor.z * 255.0f);
     
     // Each thread writes one pixel location in the texture (textel)
     pos[index].w = 0;
